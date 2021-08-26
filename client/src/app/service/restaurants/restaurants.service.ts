6e763ef63ca0241825168a8fdc263340dcfd9a27
Rybalka Yaroslav <jaroslav.rybalka@ukr.net>
2021-08-26 13:07:16 +0300

    Add restaurant service

--- client/src/app/service/restaurants/restaurants.service.ts
+++ client/src/app/service/restaurants/restaurants.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {throwError} from "rxjs";
+import {Restaurants} from "../../model/restaurant";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,11 @@ import {throwError} from "rxjs";
 
 export class RestaurantsService {
   baseUrl: string = "http://localhost:8080/api/restaurants";
-  private headers: HttpHeaders = new HttpHeaders({ Authorization: 'Basic ' + btoa('admin@gmail.com' + ':' + 'admin') });
 
   constructor(private http: HttpClient) {
   }
 
-  private handleError(error: HttpErrorResponse) {
+  private static handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       console.error('An error occured:', error.error.message);
     } else {
@@ -25,5 +26,29 @@ export class RestaurantsService {
         'Something bad happened; please try again later.'
       );
     }
+  }
+
+  list(): any {
+    const headers = new HttpHeaders({Authorization: 'Basic ' + btoa('admin@gmail.com' + ':' + 'admin')});
+    return this.http.get<Restaurants>(`${this.baseUrl}`, {headers})
+      .pipe(
+        catchError(RestaurantsService.handleError)
+      );
+  }
+
+  getById(id: number): any {
+    const headers = new HttpHeaders({Authorization: 'Basic ' + btoa('admin@gmail.com' + ':' + 'admin')});
+      return this.http.get(`${this.baseUrl}/${id}`, {headers})
+        .pipe(
+          catchError(RestaurantsService.handleError)
+        );
+  }
+
+  add(restaurant: Restaurants): any {
+    const headers = new HttpHeaders({Authorization: 'Basic ' + btoa('admin@gmail.com' + ':' + 'admin')});
+    return this.http.post(`${this.baseUrl}`, restaurant, {headers})
+      .pipe(
+        catchError(RestaurantsService.handleError)
+      )
   }
 }
--- server/src/main/java/com/myproject/restaurantvoting/config/SecurityConfig.java
+++ server/src/main/java/com/myproject/restaurantvoting/config/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/restaurants/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
                 .antMatchers(HttpMethod.POST, "/api/restaurants/**").hasRole(Role.ADMIN.name())
                 .antMatchers(HttpMethod.PUT, "/api/restaurants/**").hasRole(Role.ADMIN.name())
